ff0197ee89183d562086d8718991e98046a3262b
Hietan <hietan.code@gmail.com>
2024-04-24 22:11:04 +0900

    add headless option

--- src/accprob/test.py
+++ src/accprob/test.py
@@ -3,20 +3,23 @@ from selenium.webdriver.common.by import By
 import time
 import subprocess
 
-driver = webdriver.Chrome()
-driver.get('https://kenkoooo.com/atcoder/#/contest/show/e4b1a4f8-2043-4d70-8437-663152a8b700')
+url = 'https://kenkoooo.com/atcoder/#/contest/show/e4b1a4f8-2043-4d70-8437-663152a8b700'
 
-time.sleep(1)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+
+time.sleep(0.5)
 
 links = driver.find_elements(By.XPATH, '//table//td/a')
 
-# url = elem[0].get_attribute('href')
-# for l in links:
-#     contest_id, problem_num = l.get_attribute('href').split('/')[-1].rsplit('_', 1)
-#     print(contest_id, problem_num)
+for l in links:
+    contest_id, problem_num = l.get_attribute('href').split('/')[-1].rsplit('_', 1)
+    print(contest_id, problem_num)
 
-contest_id, problem_num = links[0].get_attribute('href').split('/')[-1].rsplit('_', 1)
-subprocess.run(['acc', 'new', contest_id])
-
+# contest_id, problem_num = links[0].get_attribute('href').split('/')[-1].rsplit('_', 1)
+# subprocess.run(['acc', 'new', contest_id])
 
 driver.close()
