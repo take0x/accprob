ef361da2ca4829ef6c9bc2c31c19fe6caab7d9f9
take0x <89313929+take0x@users.noreply.github.com>
2024-04-24 15:37:48 +0900

    Add web scraping functionality to CLI

--- src/accprob/cli.py
+++ src/accprob/cli.py
@@ -1,5 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def main() -> None:
-    print("Hello from accprob!")
+    url = input("URL: ")
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    title = soup.find("title")
+    print(title.text)
 
 
 if __name__ == "__main__":
