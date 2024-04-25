0d5dbb8d436882e5b9e2fcc6302bb70927c2dd27
Hietan <hietan.code@gmail.com>
2024-04-25 10:08:08 +0900

    Implemented a function to download individual test cases

--- src/accprob/test.py
+++ src/accprob/test.py
@@ -17,14 +17,12 @@ time.sleep(3)
 contest_title = driver.find_element(By.XPATH, '//h1').text
 links = driver.find_elements(By.XPATH, '//table//td/a')
 
-subprocess.run(['mkdir', contest_title])
-
 for ind, l in enumerate(links):
     contest_id = l.get_attribute('href').split('/')[-3]
     problem_id = l.get_attribute('href').split('/')[-1]
     problem_dir = contest_title + '/' + format(ind + 1, '02') + '/test'
     problem_url = f'https://atcoder.jp/contests/{contest_id}/tasks/{problem_id}'
-    subprocess.run(['mkdir', problem_dir])
-    subprocess.run(['oj', 'd', '-d', problem_dir, problem_url])
+    subprocess.run(['oj', 'd', '-d', problem_dir, problem_url], stdout=subprocess.DEVNULL)
+    print('Downloaded the test case :', format(ind + 1, '02'), problem_id)
 
 driver.close()
